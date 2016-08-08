947835ee691849e1c74160cdfd026d6d013de0e9
daniel bartel <dan.m.bartel@gmail.com>
2016-08-08 00:24:29 -0500

    WIP Add converter for making poetry writin' easier

---
+++ _plugins/poem_converter.rb
@@ -0,0 +1,21 @@
+## Converter which makes poetry syntax easier to write
+
+module Jekyll
+  class PoemConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /^\.poem$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      # TODO
+      "<div class='poem'>#{content.split("\n").join("<br>")}</div>"
+    end
+  end
+end
